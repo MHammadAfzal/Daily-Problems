103c3c2305a19aed581e1355259ec2888210d62c
M Hammad <hammadafzal1111@gmail.com>
2023-05-16 06:58:36 -0700

    Documentation was done

--- dp1_sayHello.py
+++ dp1_sayHello.py
@@ -1,2 +1,7 @@
+# This code is checking if the current script is being run as the main program (as opposed to being
+# imported as a module into another program). If it is being run as the main program, it will print
+# "Hello, World" to the console. This is a common way to include test code or example code in a module
+# that can be run directly for testing or demonstration purposes.
+
 if __name__ == "__main__":
     print("Hello, World")
--- dp2_division.py
+++ dp2_division.py
@@ -1,3 +1,8 @@
+# This code block is checking if the current script is being run as the main program (as opposed to
+# being imported as a module into another program). If it is being run as the main program, it prompts
+# the user to input values for variables `a` and `b`, then prints the result of integer division (`a
+# / / b`) and regular division (`a / b`).
+
 if __name__ == "__main__":
     a = int(input("What's a? "))
     b = int(input("What's b? "))
